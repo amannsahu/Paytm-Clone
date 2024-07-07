164fe8102a16522d7860bdb39f0e1b4761454e72
amannsahu <162126819+amannsahu@users.noreply.github.com>
2024-07-07 11:02:36 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors: {
+        lightBlue: "#e0f5fe",
+        LightBlue400: "#00b9f1",
+        darkBlue: "#002E6E"
+
+      }
+    },
+  },
+  plugins: [],
+}
+
